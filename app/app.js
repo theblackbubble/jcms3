8f9e0f8638a4604264532f231febf9da12019d0f
anibalsanchez <anibal.sanchez@extly.com>
2016-02-20 11:36:38 +0100

    Content-Security-Policy

--- app/app.js
+++ app/app.js
@@ -23,27 +23,32 @@ import {TabsPage} from './pages/tabs/tabs';
 
 @App({
   template: '<ion-nav [root]="rootPage"></ion-nav>',
-  config: {} // http://ionicframework.com/docs/v2/api/config/Config/
+
+  // http://ionicframework.com/docs/v2/api/config/Config/
+  config: {} 
 })
 export class MyApp {
   constructor(platform: Platform) {
     this.rootPage = TabsPage;
 
     platform.ready().then(() => {
-      // The platform is now ready. Note: if this callback fails to fire, follow
-      // the Troubleshooting guide for a number of possible solutions:
-      //
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      //
-      // First, let's hide the keyboard accessory bar (only works natively) since
-      // that's a better default:
-      //
-      // Keyboard.setAccessoryBarVisible(false);
-      //
-      // For example, we might change the StatusBar color. This one below is
-      // good for dark backgrounds and light text:
-      // StatusBar.setStyle(StatusBar.LIGHT_CONTENT)
+		// The platform is now ready. Note: if this callback fails to fire, follow
+		// the Troubleshooting guide for a number of possible solutions:
+		//
+		// Okay, so the platform is ready and our plugins are available.
+		// Here you can do any higher level native things you might need.
+		//
+		// First, let's hide the keyboard accessory bar (only works natively) since
+		// that's a better default:
+		//
+		// Keyboard.setAccessoryBarVisible(false);
+		//
+		// For example, we might change the StatusBar color. This one below is
+		// good for dark backgrounds and light text:
+		// StatusBar.setStyle(StatusBar.LIGHT_CONTENT)
+		
+		// Angular 2 is running in the development mode. Call enableProdMode()
+		// to enable the production mode.
     });
   }
 }
